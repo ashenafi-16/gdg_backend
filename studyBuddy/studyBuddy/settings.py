ba15a2a0b9c00e1feeb32f24058291a17946fa3a
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2025-05-06 16:20:29 +0300

    add requirements.txt

--- studyBuddy/studyBuddy/settings.py
+++ studyBuddy/studyBuddy/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'buddyback.onrender.com',
-    # 'gdg-backend-4.onrender.com',
+    'gdg-backend-4.onrender.com',
     'gdg-backend-9.onrender.com',  
     'localhost',
     '127.0.0.1',
